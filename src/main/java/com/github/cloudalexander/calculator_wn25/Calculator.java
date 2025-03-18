c6e8ed9eec00035d24a728ab84cc9b88127b72b0
Sergio Santana <sergsantana777@gmail.com>
2025-03-18 15:34:43 -0700

    Adds initial Calculator code

--- src/main/java/com/github/cloudalexander/calculator_wn25/Calculator.java
+++ src/main/java/com/github/cloudalexander/calculator_wn25/Calculator.java
@@ -1,12 +1,17 @@
 package com.github.cloudalexander.calculator_wn25;
 
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
     }
 
     public int divide(int a, int b) {
